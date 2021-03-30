47d8860e63b8910d89bbf8cf9216127967598dd3
Juan Pedro Pérez Alcántara <jp.perez.alcantara@gmail.com>
2021-03-30 20:35:52 +0200

    feat(holistic_hornet review)

--- docker-tags/holistic_hornet/020_production/packages/create_user.sh
+++ docker-tags/holistic_hornet/020_production/packages/create_user.sh
@@ -3,4 +3,4 @@
 # Creation of postgres user and group
 mkdir -p /var/lib/postgresql
 groupadd -g $POSTGRESGROUPID postgres
-useradd -u $POSTGRESUSERID --home '/var/lib/postgresql' -g postgres postgres
+useradd -u $POSTGRESUSERID -m -d '/var/lib/postgresql' -g postgres postgres
--- docker-tags/holistic_hornet/020_production/packages/run_psql.sh
+++ docker-tags/holistic_hornet/020_production/packages/run_psql.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-mkdir -p /var/lib/postgresql
-touch /var/lib/postgresql/.psql_history
-
 if [ "$POSTGRESGROUPID" -ne "0" ]; then
 
   groupadd -g $POSTGRESGROUPID postgres
-  chown -R $POSTGRESUSERID:$POSTGRESGROUPID /var/lib/postgresql/
 
 fi
 
 if [ "$POSTGRESUSERID" -ne "0" ]; then
 
-  useradd -u $POSTGRESUSERID --home '/var/lib/postgresql' -g postgres postgres
+  useradd -u $POSTGRESUSERID -m -d '/var/lib/postgresql' -g postgres postgres
   chown -R $POSTGRESUSERID:$POSTGRESGROUPID /var/lib/postgresql/
+  touch /var/lib/postgresql/.psql_history
 
 fi
 
@@ -21,6 +18,7 @@ if [ "$POSTGRESGROUPID" -eq "0" ]; then
 
   if [ "$POSTGRESUSERID" -eq "0" ]; then
 
+    touch /root/.psql_history
     PGPASSWORD=${PASS_F} psql -h ${HOST_F} -p ${PORT_F} -U ${USER_F} ${DB_F} ${SCRIPT} ${COMMAND} ${OUTPUT_FILES_F}
 
   fi
