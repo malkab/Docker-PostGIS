96e74b56c4be9d7030cb0ded9cefd84abc3d9d7f
Malkab <jp.perez.alcantara@gmail.com>
2025-01-24 08:21:40 +0100

    wip

--- docker-tags/karmic_kakarot/env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export DOCKER_IMAGE_TAG=karmic_kakarot
-
-export GDAL_VERSION=3.9.2
-export GEOS_VERSION=3.12.2
-export POSTGIS_VERSION=3.4.2
-export PG_VERSION=16.4
-export PROJ_VERSION=9.1.1
